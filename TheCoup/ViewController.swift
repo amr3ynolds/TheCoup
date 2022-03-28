fb9c2c284323a2219dd4076082758e818c4b7cc0
Ritter <ritteramsbaugh@gmail.com>
2022-03-27 17:07:49 -0700

    blah

--- TheCoup/ViewController.swift
+++ TheCoup/ViewController.swift
@@ -12,6 +12,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+    }
+
+
+    @IBAction func Square(_ sender: Any) {
+        
+    }
+    @IBOutlet weak var check: UIImageView!
+    @IBAction func Button(_ sender: Any) {
+        
+        if check.isHidden == true {
+            
+            check.isHidden = false
+        } else {
+            check.isHidden = true
+        }
+        
+    }
+    
+    @IBAction func Button2(_ sender: Any) {
+        
     }
 
 
